dd31a170ca9d13ea230bf358ba2bb3df95d6eba6
szabi1112 <banki.szabolcs@gmail.com>
2022-09-06 22:57:50 +0200

    Kész

--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,17 @@
+const sortStrings = (arr = []) => new Promise((resolve, reject) => {
+  let newArr;
+  let arrayOrNot;
+  try {
+    arrayOrNot = arr.every((item) => typeof item === 'string');
+  } catch (e) {
+   throw Error('Error: Something went wrong with sorting words!');
+  }
+  if (arrayOrNot) {
+    newArr = arr.sort((a, b) => a.localeCompare(b));
+    resolve(newArr);
+  } else {
+    reject(new Error('Error: Not all items in the array are strings!'));
+  }
+});
+
 export default sortStrings;
